dabe50ae2a402e73b5eed994b9eac7e6579d2cfd
James <j.c.searson@gmail.com>
2015-04-07 16:18:02 -0400

    Added Favicons

--- parts/doctype.php
+++ parts/doctype.php
@@ -4,12 +4,24 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
+	<!-- Favicons -->
+	<link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon-180x180.png">
 	<!-- Metas -->
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,Chrome=1">
 	<title>Steven E. Caplan, M.D.</title>
       	<meta name="description" content="The Template for Steven Caplan, MD">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Typekit Font Request -->
+	<script>
+	  (function(d) {
+	    var config = {
+	      kitId: 'ajj6lkc',
+	      scriptTimeout: 3000
+	    },
+	    h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='//use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+	  })(document);
+	</script>
 	<!-- Stylesheets -->
 	<link rel="stylesheet" type="text/css" href="css/style.css?<?php echo filemtime(TEMPLATEPATH . 'css/style.css'); ?>" />
 </head>
