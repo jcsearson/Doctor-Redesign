f7d145b06f18a1f3b089ed3c57bf1920905f8728
James <j.c.searson@gmail.com>
2015-04-07 14:29:41 -0400

    Styled and Modified Appearance

--- about.php
+++ about.php
@@ -1,7 +1,5 @@
 <?php include("parts/doctype.php"); ?>
 
-<body>
-
 	<div class="top-content">
 		<div class="head-container">
 			<?php include("parts/header.php"); ?>
@@ -16,75 +14,80 @@
 				<p><em>It is important to us that you feel comfortable from the moment you step through our doors to the time that you leave us.  Our outstanding staff will make this time an easy one for both the child and parent!</em></p>
 			</section>  <!-- .main-info-upper   .main-info  .staff-entry  -->
 
-			<div class="main-divider"></div>
-
 			<section class="main-info-lower main-info staff-entry" role="region">
-				<h2 class="staff-title">Dr. Steven E. Caplan <i>M.D.</i></h2>
-				<div class="thumb-container steve"></div>
+				<div class="staff-head-block">
+					<h2 class="staff-title">Dr. Steven E. Caplan <span class="credentials"><i>M.D.</i></span></h2>
+					<div class="thumb-container steve"></div>
+					<div class="head-strike"></div>
+				</div>
 				<p>Dr. Caplan graduated from the University of Rochester, School of Medicine in 1975.  He completed an internship and residency in pediatrics at the University of Rochester in 1977, and a chief residency and fellowship in ambulatory pediatrics at Sinai Hospital (Baltimore) in 1979.  Dr. Caplan became <em>Board Certified in Pediatrics</em> in 1979.</p>
 				<p class="staff-bar">Dedication to providing the highest quality medical care for all children remains the cornerstone to Dr. Caplan's practice.  He is the Maryland coordinator of PROS, the office-based collaborative research network of the American Academy of Pediatrics.  Dr. Caplan is the past <em>President of the Sinai Hospital Medical Staff</em> and remains active on several hospital committees.</p>
 			</section>  <!-- .main-info-lower   .main-info  .staff-entry  -->
 
-			<div class="main-divider"></div>
-
 			<section class="main-info-lower main-info staff-entry" role="region">
-				<h2 class="staff-title">Catherine M. Searson <i>CNRP</i>, <i>RN</i></h2>
-				<div class="thumb-container catherine"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
-				<p class="staff-bar">Mrs. Searson received a BS degree in nursing from Boston University and went on to obtain an Masters degree in pediatric primary care (<em>Pediatric Nurse Practitioner</em>) from the University of Maryland at Baltimore in 1996.  She has practiced as a Pediatric Nurse Practitioner at our practice since 1998 with particular focus on the needs of the female adolescent.</p>
+				<div class="staff-head-block">
+					<h2 class="staff-title">Catherine M. Searson <span class="credentials"><i>CNRP</i>, <i>RN</i></span></h2>
+					<div class="thumb-container catherine"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+					<div class="head-strike"></div>
+				</div>
+				<p class="staff-bar">Mrs. Searson received a BS degree in nursing from Boston University and went on to obtain a Masters degree in pediatric primary care (<em>Pediatric Nurse Practitioner</em>) from the University of Maryland at Baltimore in 1996.  She has practiced as a Pediatric Nurse Practitioner at our practice since 1998 with particular focus on the needs of the female adolescent.</p>
 			</section>  <!-- .main-info-lower   .main-info  .staff-entry  -->
 
-			<div class="main-divider"></div>
-
 			<section class="main-info-lower main-info staff-entry" role="region">
-				<h2 class="staff-title">Karen Mandwelle Caplan <i>LCSW-C</i></h2>
-				<div class="thumb-container karen"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+				<div class="staff-head-block">
+					<h2 class="staff-title">Karen M. Caplan <span class="credentials"><i>LCSW-C</i></span></h2>
+					<div class="thumb-container karen"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+					<div class="head-strike"></div>
+				</div>
 				<p class="staff-bar">Mrs. Caplan received her Master of Social Work from the Catholic University of America, in Washington D.C.  She is a certified, licensed <em>clinical social worker</em> whom specializes in the treatment of adolescents and young adults.</p>
 			</section>  <!-- .main-info-lower   .main-info  .staff-entry  -->
 
-			<div class="main-divider"></div>
-
 			<section class="main-info-lower main-info staff-entry" role="region">
-				<h2 class="staff-title">Wendy Miller, <i>IBCLC</i></h2>
-				<div class="thumb-container wendy"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+				<div class="staff-head-block">
+					<h2 class="staff-title">Wendy Miller, <span class="credentials"><i>IBCLC</i></span></h2>
+					<div class="thumb-container wendy"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+					<div class="head-strike"></div>
+				</div>
 				<p>Mrs. Miller is a Baltimore native who has worked with Dr. Caplan since 1984.  She has been <em>office manager</em> since the practice was opened in1988 and will assist you with all your insurance and billing issues.</p>
 				<p class="staff-bar">Wendy continues to seek educational and professional advancement.  In 1993 she became a Board <em>Certified Lactation Consultant</em>, helping mothers successfully breastfeed and transition through back-to-work issues and weaning.  In 2009 she became an Emergency Medical Technician (EMT) and I.V. Technician.  In 2014 Wendy  qualified as a Registered Nurse in the state of Maryland.</p>
 			</section>  <!-- .main-info-lower   .main-info  .staff-entry  -->
 
-			<div class="main-divider"></div>
-
 			<section class="main-info-lower main-info staff-entry" role="region">
-				<h2 class="staff-title">Ms. DeVonn Thornton</h2>
-				<div class="thumb-container devonn"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+				<div class="staff-head-block">
+					<h2 class="staff-title">Ms. DeVonn Thornton</h2>
+					<div class="thumb-container devonn"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+					<div class="head-strike"></div>
+				</div>
 				<p class="staff-bar">Ms. Thornton is a <em>Certified Medical Assistant</em> and <em>Certified Phlebotomist</em> with 14 years experience.  She manages the examination rooms, checks patients' temperature, blood pressure and weight, gives vaccinations and draws blood for testing from even the youngest of babies.  Occasionally Ms. Thornton may assist with front office activities.</p>
 			</section>  <!-- .main-info-lower   .main-info  .staff-entry  -->
 
-			<div class="main-divider"></div>
-
 			<section class="main-info-lower main-info staff-entry" role="region">
-				<h2 class="staff-title">Kimberly Carroll</h2>
-				<div class="thumb-container kimberly"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+				<div class="staff-head-block">
+					<h2 class="staff-title">Kimberly Carroll</h2>
+					<div class="thumb-container kimberly"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+					<div class="head-strike"></div>
+				</div>
 				<p class="staff-bar">Ms. Carroll is the mother of three children.  She has worked as a <em>Certified Medical Assistant</em> for nine years of which six were spent working with Dr. Ralph Brown.  Kim has worked in both Family Medicine and Pediatric Primary Care but loves working with children most.  She is currently a student at CCBC studying political science and hopes to become a human rights activist. </p>
 			</section>  <!-- .main-info-lower   .main-info  .staff-entry  -->
 
-			<div class="main-divider"></div>
-
 			<section class="main-info-lower main-info staff-entry" role="region">
-				<h2 class="staff-title">Vanessa Loney, <i>RMA</i></h2>
-				<div class="thumb-container vanessa"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+				<div class="staff-head-block">
+					<h2 class="staff-title">Vanessa Loney, <span class="credentials"><i>RMA</i></span></h2>
+					<div class="thumb-container vanessa"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+					<div class="head-strike"></div>
+				</div>
 				<p class="staff-bar">Vanessa joined Dr. Caplan's team in September as a <em>Certified Medical Assistant</em>  from Fortis Institute, Towson.  She is excited to provide excellent quality care to each and every patient.</p>
 			</section>  <!-- .main-info-lower   .main-info  .staff-entry  -->
 
-			<div class="main-divider"></div>
-
 			<section class="main-info-lower main-info staff-entry" role="region">
-				<h2 class="staff-title">Candice A. Fabian</h2>
-				<div class="thumb-container candice"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+				<div class="staff-head-block">
+					<h2 class="staff-title">Candice A. Fabian</h2>
+					<div class="thumb-container candice"><?php echo file_get_contents("images/svg/missing-thumb.svg"); ?></div>
+					<div class="head-strike"></div>
+				</div>
 				<p>Ms. Fabian returned to Dr. Caplan's practice in January 2013 after an 11 year hiatus!  During that time she trained and practiced as a Doula (<em>labor assistant</em>), childbirth educator and home-based Waldorf teacher.  She is thrilled to be back "home" working as our front-line person managing the reception area, answering your calls and directing you to the right person. When needed, Candice can also work as a medical assistant.</p>
 			</section>  <!-- .main-info-lower   .main-info  .staff-entry  -->
 		</main>
 	</div>  <!-- .top-content -->
 
-	<?php include("parts/footer.php"); ?>
-
-</body>
-</html>
+<?php include("parts/footer.php"); ?>
