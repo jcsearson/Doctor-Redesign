242e3b2356c131f27372c91527279c7b91e7d687
James <j.c.searson@gmail.com>
2015-04-08 15:07:15 -0400

    Added Custom Post Type -- Resources

--- functions.php
+++ functions.php
@@ -50,4 +50,56 @@ $args = array(
 add_theme_support('post-thumbnails');
 register_post_type( 'employees', $args );
 }
+
+/* Resources -- Custom Post Type */
+add_action( 'init', 'register_cpt_resources' );
+function register_cpt_resources() {
+$labels = array(
+'name' => _x( 'Resources', 'resources' ),
+'singular_name' => _x( 'Resource', 'resource' ),
+'add_new' => _x( 'Add New', 'add new' ),
+'add_new_item' => _x( 'Add New Resource', 'add new resource' ),
+'edit_item' => _x( 'Edit Resources', 'edit resources' ),
+'new_item' => _x( 'New Resource', 'new resource' ),
+'view_item' => _x( 'View Resource', 'view resource' ),
+'search_items' => _x( 'Search Resources', 'search resources' ),
+'not_found' => _x( 'Resource was Not Found', 'resource was not found' ),
+'not_found_in_trash' => _x( 'Not found in Trash', 'not found in trash' ),
+'parent_item_colon' => _x( 'Parent Resource:', 'parent resource:' ),
+'menu_name' => _x( 'Resources', 'resources' ),
+);
+$args = array(
+'labels' => $labels,
+'hierarchical' => true,
+'description' => 'Custom post type for Resources.',
+'supports' => array( 	'title',
+						'editor',
+						'excerpt',
+						'author',
+						'thumbnail',
+						'trackbacks',
+						'custom-fields',
+						'comments',
+						'revisions',
+						'page-attributes',
+						'post-formats'),
+'taxonomies' => array( 'category',
+						'post_tag',
+						'page-category' ),
+'public' => true,
+'show_ui' => true,
+'show_in_menu' => true,
+'menu_icon' => 'dashicons-book',
+'show_in_nav_menus' => true,
+'publicly_queryable' => true,
+'exclude_from_search' => false,
+'has_archive' => true,
+'query_var' => true,
+'can_export' => true,
+'rewrite' => true,
+'capability_type' => 'post'
+);
+add_theme_support('post-thumbnails');
+register_post_type( 'resources', $args );
+}
 ?>
