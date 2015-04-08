c320d4b203bc3b6a49534e56cf132648cfea2d57
James <j.c.searson@gmail.com>
2015-04-08 02:48:29 -0400

    Resources Page, Styling, New Patients Page

---
+++ resources.php
@@ -0,0 +1,28 @@
+<?php include("parts/doctype.php"); ?>
+
+	<?php include("parts/header.php"); ?>
+	<?php include("parts/nav.php"); ?>
+
+	<?php include("parts/sidebar.php"); ?>
+
+	<section class="main-info-upper main-info" role="region">
+		<h2>Helpful Information:</h2>
+		<p>Below are some resources that can provide helpful insite regarding various aspects of childcare.  Please feel free to browse through them if you want additional information.</p>
+	</section>  <!-- .main-info-upper   .main-info -->
+
+	<section class="resource-container" role="region">
+		<div class="single-resource">
+			<h4 class="resource-name"><a href="http://www.webmd.com/" class="resource-link" target="_blank">Resource#1</a></h4>
+			<p class="resource-description">A description of what this resource can be used for and any other pertinent information.</p>
+		</div>
+		<div class="single-resource">
+			<h4 class="resource-name"><a href="http://www.medicinenet.com/script/main/hp.asp" class="resource-link" target="_blank">Resource#2</a></h4>
+			<p class="resource-description">A description of what this resource can be used for and any other pertinent information.</p>
+		</div>
+		<div class="single-resource">
+			<h4 class="resource-name"><a href="http://www.hrsa.gov/index.html" class="resource-link" target="_blank">Resource#3</a></h4>
+			<p class="resource-description">A description of what this resource can be used for and any other pertinent information.</p>
+		</div>
+	</section>  <!-- .main-info-lower   .main-info -->
+
+<?php include("parts/footer.php"); ?>
