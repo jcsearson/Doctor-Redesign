8c10a4606f6f5a7aeb676dbc918d70e7ddc8b975
James <j.c.searson@gmail.com>
2015-04-12 00:17:34 -0400

    New Mobile Nav Layout, Styled Announcement Headers

--- parts/nav.php
+++ parts/nav.php
@@ -3,34 +3,30 @@
 			<li class="navigation-item">
 				<a href="/" class="nav-txt-item">Home</a>
 				<a href="/" class="svg-anchor"><?php echo file_get_contents("images/svg/home.svg"); ?></a>
-			</li>
+			</li>  <!-- .navigation-item -->
 			<li class="navigation-item">
 				<a href="/about.php" class="nav-txt-item">About</a>
 				<a href="/about.php" class="svg-anchor"><?php echo file_get_contents("images/svg/about-us.svg"); ?></a>
-			</li>
+			</li>  <!-- .navigation-item -->
 			<li class="navigation-item">
 				<a href="/contact.php" class="nav-txt-item">Contact</a>
 				<a href="/contact.php" class="svg-anchor"><?php echo file_get_contents("images/svg/contact.svg"); ?></a>
-			</li>
-			<li class="navigation-item dropdown-ignition">
+			</li>  <!-- .navigation-item -->
+			<li class="navigation-item">
 				<a href="/payment.php" class="nav-txt-item">Pay Online</a>
-				<a href="#" class="svg-anchor drop-marker"><?php echo file_get_contents("images/svg/more.svg"); ?></a>
-				<ul class="dropdown-menu">
-					<li class="dropdown-item"><a href="/payment.php">Pay Online</a></li>
-					<li class="dropdown-item"><a href="/new-patients.php">New Patients</a></li>
-					<li class="dropdown-item"><a href="/after-hours.php">After-hours</a></li>
-					<li class="dropdown-item"><a href="/resources.php">Resources</a></li>
-				</ul>  <!-- .dropdown-menu -->
-			</li>  <!-- .navigation-item  .dropdown-ignition -->
-			<li class="navigation-item item-to-dropdown">
-				<a href="/new-patients.php" class="nav-txt-item">New Patients</a>
-			</li>
-			<li class="navigation-item item-to-dropdown">
+				<a href="/payment.php" class="svg-anchor"><?php echo file_get_contents("images/svg/pay.svg"); ?></a>
+			</li>  <!-- .navigation-item -->
+			<li class="navigation-item">
 				<a href="/after-hours.php" class="nav-txt-item">After-hours</a>
-			</li>
-			<li class="navigation-item item-to-dropdown">
+				<a href="/after-hours.php" class="svg-anchor"><?php echo file_get_contents("images/svg/after-hours.svg"); ?></a>
+			</li>  <!-- .navigation-item -->
+			<li class="navigation-item">
 				<a href="/resources.php" class="nav-txt-item">Resources</a>
-			</li>
+				<a href="/resources.php" class="svg-anchor"><?php echo file_get_contents("images/svg/resources.svg"); ?></a>
+			</li>  <!-- .navigation-item -->
+			<li class="navigation-item anchor-item">
+				<a href="/new-patients.php" class="nav-txt-item">New Patients</a>
+			</li>  <!-- .navigation-item  -->  <!-- .anchor-item -->
 		</ul>  <!-- .navigation-menu -->
 	</nav>
 
