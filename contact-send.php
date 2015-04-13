9b2b09d4915dac788ec14f04f5ca11050aa87fba
James <j.c.searson@gmail.com>
2015-04-13 01:24:09 -0400

    Added checks to contact form, makes sure all fields are filled in and they are valid.

--- contact-send.php
+++ contact-send.php
@@ -12,10 +12,40 @@
 
 	<section class="contact-submission-container">
 		<?php
-			$first_name = $_POST[ 'first_name' ];
-			$middle_name = $_POST[ 'middle_name' ];
-			$last_name = $_POST[ 'last_name' ];
-			$email = $_POST[ 'email' ];
+			/* Assign inputs to variables, then test all inputs for proper completion, then do nothing if passes test. */
+			/* Check that a first name was entered and that it was valid. */
+			if (empty($_POST[ 'first_name' ])) {
+				$emptyNameError = "A first name is required.";
+			} else {
+				$first_name = test_input($_POST[ 'first_name' ]);
+				if (!preg_match("/^[a-zA-Z ]*$/	", $first_name)) {
+					$wrongNameError = "Only letters and spaces allowed.";
+				}
+			}
+			/* Optional - Only check to make sure its letters - not characters. */
+			$middle_name = test_input($_POST[ 'middle_name' ]);
+			if (!preg_match("/^[a-zA-Z ]*$/	", $middle_name)) {
+				$nameError = "Only letters and spaces allowed.";
+			}
+			/* Check that a last name was entered and that it was valid. */
+			if (empty($_POST[ 'last_name' ])) {
+				$emptyNameError = "A last name is required."
+			} else {
+				$last_name = test_input($_POST[ 'last_name' ]);
+				if (!preg_match("/^[a-zA-Z ]*$/	", $last_name)) {
+					$nameError = "Only letters and spaces allowed.";
+				}
+			}
+			/* Check that an email was entered and it is a valid email address. */
+			if (empty($_POST[ 'email' ])) {
+				$emptyEmailError = "Please enter an email address.";
+			} else {
+				$email = test_input($_POST[ 'email' ]);
+				if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	  				$emailError = "The email you entered is not valid.";
+				}
+			}
+			/* No check needed, default value set to "No". */
 			$patient = $_POST[ 'patient' ];
 
 			/* Combines 3 parts of Phone input into 1 for Message */
