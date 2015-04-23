ae305b9c81e0091cbbe390ff407d236118a1e61d
James <j.c.searson@gmail.com>
2015-04-23 03:20:39 -0400

    Adjusted site disclaimer layout -- Adjusted responsiveness of borders on payment page.

--- disclaimer.php
+++ disclaimer.php
@@ -7,13 +7,13 @@
 
 	<section class="main-info-upper main-info" role="region">
 		<h2>Site Disclaimer:</h2>
-		<p><em>Steven E.  Caplan M.D., PA.</em> offers this site as a service which is intended to provide only general information and educational resources.  Nothing related to this site is intended as medical advice nor should be used for diagnosis or treatment.  Furthermore, this site is not intended to create any physician-patient relationship, nor should it be considered a replacement for consultation with a healthcare professional.</p>
+		<p><strong>Steven E.  Caplan M.D., PA.</strong> offers this site as a service which is intended to provide only general information and educational resources.  Nothing related to this site is intended as medical advice nor should be used for diagnosis or treatment.  Furthermore, this site is not intended to create any physician-patient relationship, nor should it be considered a replacement for consultation with a healthcare professional.</p>
 	</section>  <!-- .main-info-upper   .main-info -->
-
-	<div class="main-divider"></div>
 
 	<section class="main-info-lower main-info" role="region">
 		<p>If you have questions or concerns about your health, please contact your healthcare professional directly.  Secondly, reading the information in the provided links does not imply a recommendation or endorsement.  The information is not a substitute for a qualified health professional's recommendations.  Any patient seeking specific recommendations must consult a qualified healthcare professional for a diagnosis and recommended treatment.</p>
 	</section>  <!-- .main-info-lower   .main-info -->
+
+	<div class="main-divider"></div>
 
 <?php include("parts/footer.php"); ?>
