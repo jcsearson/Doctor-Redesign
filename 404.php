4bb8f4f28d639182ad93cc5e439bb9640363c2b1
James <j.c.searson@gmail.com>
2015-04-28 11:53:21 -0400

    Added 404 page.

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+	/*
+ 	* The template for displaying 404 pages (Not Found).
+	*/
+?>
+
+<?php get_header(); ?>
+
+	<?php get_sidebar(); ?>
+
+	<section class="main-info-upper main-info" role="region">
+		<h2>Oops!</h2>
+		<p>The page you are looking for does not exist!  Please try refreshing the browser or navigating back to another page.</p>
+	</section>  <!-- .main-info-upper   .main-info -->
+
+<?php get_footer(); ?>
